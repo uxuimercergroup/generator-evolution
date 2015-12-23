c76b3adaec815b4fda0aa5c18f4836b624f6ea2c
Matthew Holmes <matthew.holmes@mercer.com>
2015-12-23 11:47:20 -0500

    generator sftp deploy dest commented out, change at project level

--- generators/app/templates/grunt/sftp-deploy.js
+++ generators/app/templates/grunt/sftp-deploy.js
@@ -7,7 +7,7 @@ module.exports = {
       authKey: 'key1'
     },
     src: 'dist/',
-    dest: '/www/content/uxui/docs/assemble_test',
+    // dest: '/www/content/uxui/docs/path/to/project/dir',
     serverSep: '/',
     concurrency: 3,
     progress: true
