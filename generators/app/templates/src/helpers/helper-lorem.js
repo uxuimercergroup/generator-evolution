2047d3ac1f1f6ce2daa0c1a68a4013dd3e26fe79
Doug Fraize <dug.fraize@gmail.com>
2016-03-22 13:56:23 -0400

    removed assets call on tooltip and added link in for the lorem helper

--- generators/app/templates/src/helpers/helper-lorem.js
+++ generators/app/templates/src/helpers/helper-lorem.js
@@ -3,5 +3,6 @@ module.exports.register = function (Handlebars, options)  {
 
 	// First item is the handlebars tag name to be used in page for ex: 'lorem',
 	// the second part is the name of the Handlebars helper, this is required to get the helper registered to work
+	// https://github.com/helpers/handlebars-helper-lorem
 	handlebars.registerHelper('lorem', require('handlebars-helper-lorem'));
 };
