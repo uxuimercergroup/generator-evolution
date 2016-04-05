1bad349efd02d0373e14938d368c5564aba80c3d
Doug Fraize <dug.fraize@gmail.com>
2016-04-05 14:14:02 -0400

    removed scroll pane

--- generators/app/templates/src/assets/scripts/site.js
+++ generators/app/templates/src/assets/scripts/site.js
@@ -137,34 +137,6 @@ $(function() {
 			// Dateoicker masked date input example
 			evoDatepicker('evo-datepicker-masked-example');
 
-		// SCROLL PANE
-
-			// Scroll pane example
-			evoScrollPane('evo-scroll-pane-example');
-
-			// Horizontal scroll pane example
-			evoScrollPane('evo-scroll-pane-horizontal-example',{
-				axis:'x',
-				mouseWheel:{
-					axis:'x'
-				},
-				advanced:{
-					autoExpandHorizontalScroll: true
-				}
-			});
-
-			// Horizontal + vertical scroll pane example
-			evoScrollPane('evo-scroll-pane-horizontal-vertical-example',{
-				axis:'yx',
-				advanced:{
-					autoExpandHorizontalScroll: true
-				}
-			});
-
-			// Scroll pane custom height example
-			evoScrollPane('evo-scroll-pane-custom-height-example',{
-				setHeight: 200
-			});
 
 		// SLIDER
 
